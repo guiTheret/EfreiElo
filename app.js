aa240e8720773080b4b42caed68d64ad9ffcc8eb
Robebou <guillaume.theret@efrei.net>
2021-02-02 20:34:13 +0100

    aa

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // imports
+const { info } = require('console')
 const { response } = require('express')
 const express = require('express')
 const app = express()
@@ -7,6 +8,7 @@ const mysql = require('mysql')
 const fetch = require("node-fetch")
 
 const api_lol_key = 'RGAPI-0e164945-9a84-4d44-a5d7-b3cf4dbf25cd'
+
 
 var db = mysql.createConnection({
     host: "localhost",
@@ -31,52 +33,51 @@ app.use('views', express.static(__dirname + 'views'))
 app.set('views','./views')
 app.set('view engine','ejs')
 
-async function get_player_data_from_api(info_players) {
-    console.log(info_players)
-    info_players.forEach(element => {
-        fetch(`https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${element.nom_invocateur}?api_key=${api_lol_key}`)
-        .then(async function (response)  {
-            let data =  await response.json();
-            console.log(data)
-        })
+async function get_player_data_step1(summoner) {   
+    fetch(`https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summoner}?api_key=${api_lol_key}`)
+    .then(async function(response) {
+        let data = await response.json();
+        console.log(data)
+        return data;
+    })
+}  
+async function insert_database_player_info(summoner) {
+    var data =  get_player_data_step1(summoner)
+    .then(async function () {
+        console.log("data is : " + data)
+    })
+    
+}
+
+function summoner_info_sql_to_dict(info_players_query) {
+    var info_players = []
+    info_players_query.forEach(element => {
+        var dict = {
+            nom_invocateur: "",
+            lvl: "",
+            icone: "",
+            Elo: "",
+            opgg: ""
+        };
+        dict.nom_invocateur = element.nom_invocateur;
+        dict.opgg = element.opgg;
+        dict.lvl = element.lvl;
+        dict.elo = element.elo;
+        dict.icone = `http://ddragon.leagueoflegends.com/cdn/6.3.1/img/profileicon/${element.icone}.png`;
+        info_players.push(dict);
     });
-}   
+    return info_players;
+}
+
+
 
 app.get('',(req,res) => {
-
-    const info_players = [{
-        icone : "Image",
-        nom_invocateur : "Robert",
-        lvl: "45",
-        Elo: "Master"
-    },
-    {
-        icone : "Image",
-        nom_invocateur : "Robert",
-        lvl: "45",
-        Elo: "Master"
-    },{
-        icone : "Image",
-        nom_invocateur : "Robert",
-        lvl: "45",
-        Elo: "Master"
-    },{
-        icone : "Image",
-        nom_invocateur : "Robert",
-        lvl: "45",
-        Elo: "Bronze"
-    }
-]
-    res.render('index',{info_players: info_players})
-
-    let sql = "SELECT id, nom_invocateur,opgg FROM info_players"
-    let query = db.query(sql, (err, info_players, fields) => {
-        if (err) throw err;   
-        res.render('index',{info_players: info_players})
-        get_player_data_from_api(info_players)
-        
+    let info_players = [];
+    let sql = "SELECT id, nom_invocateur,opgg,lvl,icone,elo FROM info_players"
+    let query = db.query(sql, (err, info_players_query, fields) => {
+        info_players = summoner_info_sql_to_dict(info_players_query)
+        res.render('index',{info_players: info_players}) 
     })
-
 })
 
 app.get('/register',(req,res) => {
